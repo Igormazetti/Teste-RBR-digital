fafd907b51d93fe334ad4040f81b35a8546ccfa2
Igor Mazetti de Azevedo <iigormazetti@hotmail.com>
2024-05-17 21:33:45 -0300

    feat: created a Dialog component

---
+++ frontend/components/Dialog/Dialog.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton } from "@chakra-ui/react";
+
+interface DialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function Dialog({ isOpen, onClose, children }: DialogProps) {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>{children}</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
